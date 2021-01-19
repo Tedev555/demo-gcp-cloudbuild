9108b351ed27690c60ef7608ae0e68651ea4f7f2
Ted555 <thanong984@gmail.com>
2021-01-19 09:25:41 +0700

    First commit

---
+++ src/main/java/com/example/demogcpcloudbuild/DemoGcpCloudbuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogcpcloudbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGcpCloudbuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGcpCloudbuildApplication.class, args);
+    }
+
+}
